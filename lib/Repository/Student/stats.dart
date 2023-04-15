adf76309d490ade1f357b20ea2f0a8b69192ee34
SalmanAd01 <salmanadhikari415@gmail.com>
2023-04-15 13:14:09 +0530

    showing response to view

--- lib/Repository/Student/stats.dart
+++ lib/Repository/Student/stats.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:ffi';
 
 import 'package:attendenceapp/Models/stats.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -22,8 +23,11 @@ Future<List<StatsModel>?> getStats(String monthYear) async {
   if (response.statusCode == 200) {
     final res = jsonDecode(response.body);
     final Map<String, dynamic> resMap = res["Attendance"];
-    return List<StatsModel>.from(
-        resMap.entries.map((e) => StatsModel.fromJson(e.key, e.value)));
+    final int month = res["month"];
+    final int year = res["year"];
+    return List<StatsModel>.from(resMap.entries.map(
+      (e) => StatsModel.fromJson(e.key, e.value, month, year),
+    ));
   } else {
     print(response.statusCode);
     return [];
