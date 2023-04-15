bac73e064cd59048bc72bcaf7f0670578d340d83
SalmanAd01 <salmanadhikari415@gmail.com>
2023-04-15 12:48:54 +0530

    fix stats service

--- lib/Repository/Student/stats.dart
+++ lib/Repository/Student/stats.dart
@@ -8,7 +8,7 @@ import 'package:http/http.dart' as http;
 import '../../Styles/url.dart';
 
 Future<List<StatsModel>?> getStats(String monthYear) async {
-  
+  monthYear = monthYear.replaceAll("/", "%2F");
   final url = Uri.parse("$serverURL/students/myattendance/$monthYear");
   const storage = FlutterSecureStorage();
   final token = await storage.read(key: "token");
